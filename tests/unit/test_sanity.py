065b15e9a2389fa9a7023c859741cb68f7ea33b4
Ric Evans <ric@evans-jacquez.com>
2022-12-08 15:36:13 -0600

    add unit tests

---
+++ tests/unit/test_sanity.py
@@ -0,0 +1,47 @@
+"""Test that everything is where we think it is."""
+
+import inspect
+
+from rest_server import handlers
+from rest_tools.server import RestHandler
+
+
+def test_00__rest_handlers() -> None:
+    """Dir-check all the REST handlers."""
+
+    known_handlers = {
+        handlers.MainHandler: (
+            r"/$",
+            ["post"],
+        ),
+        handlers.EventMappingHandler: (
+            r"/event/(?P<event_id>\w+)$",
+            ["get"],
+        ),
+        handlers.ScanLauncherHandler: (
+            r"/scan$",
+            ["post"],
+        ),
+        handlers.ManifestHandler: (
+            r"/scan/manifest/(?P<scan_id>\w+)$",
+            ["get", "delete", "patch"],
+        ),
+        handlers.ResultsHandler: (
+            r"/scan/result/(?P<scan_id>\w+)$",
+            ["get", "delete", "put"],
+        ),
+    }
+
+    # search for all known handlers
+    for handler, (route, methods) in known_handlers.items():
+        assert all(x in dir() for x in methods)
+        assert handler.ROUTE == route
+
+    # find
+    for _, klass in inspect.getmembers(handlers):
+        # if it's in the list, it must be a RestHandler
+        if klass in known_handlers:
+            assert issubclass(klass, RestHandler)
+        # if it's a RestHandler, it must be in the list
+        if issubclass(klass, RestHandler):
+            assert klass in known_handlers
