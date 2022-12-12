7e4728bd5ae543d0c6b21b8cefc6c8217cf6265c
Ric Evans <ric@evans-jacquez.com>
2022-12-12 15:53:04 -0600

    use `typeguard` instead of `pydantic`

--- rest_server/database/schema.py
+++ rest_server/database/schema.py
@@ -1,11 +1,13 @@
 """Collection of dataclass-based schema for the database."""
 
+import dataclasses as dc
 from typing import Any
 
-import pydantic
+from typeguard import typechecked
 
 
-@pydantic.dataclasses.dataclass
+@typechecked
+@dc.dataclass
 class ScanIDDataclass:
     """A dataclass with a scan id."""
 
@@ -13,16 +15,18 @@ class ScanIDDataclass:
     is_deleted: bool
 
 
-@pydantic.dataclasses.dataclass
+@typechecked
+@dc.dataclass
 class Result(ScanIDDataclass):
     """Encompasses the physics results for a scan."""
 
     json_result: dict[str, Any]  # actual keys/values are open to requestor
 
 
-@pydantic.dataclasses.dataclass
+@typechecked
+@dc.dataclass
 class Manifest(ScanIDDataclass):
     """Encapsulates the manifest of a unique scan entity."""
 
     event_id: str
-    progress: dict[str, Any]  # actual keys/values are open to requestor
+    progress: dict[str, Any] = dc.field(default_factory=dict)  # open to requestor
