29817bcb86d12d33d330eb8e8a2f6759a60b5d7c
Ric Evans <ric@evans-jacquez.com>
2022-12-13 11:34:21 -0600

    move "good" test actions to own functions

--- tests/integration/test_rest_routes.py
+++ tests/integration/test_rest_routes.py
@@ -67,31 +67,20 @@ async def server(
 ########################################################################################
 
 
-async def test_00(server: Callable[[], RestClient]) -> None:
-    """Test normal scan creation and retrieval."""
-    rc = server()
-    event_id = "abc123"
-
-    #
-    # EMPTY DB
-
-    # query by event id
-    resp = await rc.request("GET", f"/event/{event_id}")
-    assert not resp["scan_ids"]  # no matches
-
-    #
-    # POPULATE DB
-
+async def _launch_scan(rc: RestClient, event_id: str) -> str:
     # launch scan
     resp = await rc.request("POST", "/scan", {"event_id": event_id})
     scan_id = resp["scan_id"]  # keep around
-
     # query by event id
     resp = await rc.request("GET", f"/event/{event_id}")
     assert [scan_id] == resp["scan_ids"]
+    return scan_id  # type: ignore[no-any-return]
 
-    # LOOP:
-    for i in range(10):
+
+async def _do_progress(
+    rc: RestClient, event_id: str, scan_id: str, n: int
+) -> dict[str, Any]:
+    for i in range(n):
         progress = {"count": i, "double_count": i * 2, "count_pow": i**i}
         # update progress
         resp = await rc.request(
@@ -107,24 +96,129 @@ async def test_00(server: Callable[[], RestClient]) -> None:
         # query progress
         resp = await rc.request("GET", f"/scan/manifest/{scan_id}")
         assert progress_resp == resp
+    return progress_resp  # type: ignore[no-any-return]
 
+
+async def _send_result(
+    rc: RestClient,
+    # event_id: str,
+    scan_id: str,
+    last_known_manifest: dict[str, Any],
+) -> dict[str, Any]:
     # send finished result
-    result = {"alpha": (i + 1) ** i, "beta": -i}
+    result = {"alpha": (11 + 1) ** 11, "beta": -11}
     resp = await rc.request("PUT", f"/scan/result/{scan_id}", {"json_dict": result})
     assert resp == {
         "scan_id": scan_id,
         "is_deleted": False,
         "json_dict": result,
     }
-    result_resp = resp  # keep around
+    result = resp  # keep around
 
     # query progress
     resp = await rc.request("GET", f"/scan/manifest/{scan_id}")
-    assert progress_resp == resp
+    assert last_known_manifest == resp
 
     # query result
     resp = await rc.request("GET", f"/scan/result/{scan_id}")
-    assert result_resp == resp
+    assert result == resp
+
+    return result
+
+
+async def _delete_manifest(
+    rc: RestClient,
+    event_id: str,
+    scan_id: str,
+    last_known_manifest: dict[str, Any],
+    last_known_result: dict[str, Any],
+) -> None:
+    # delete manifest
+    resp = await rc.request("DELETE", f"/scan/manifest/{scan_id}")
+    assert resp == {
+        "scan_id": scan_id,
+        "is_deleted": True,
+        "event_id": event_id,
+        "progress": last_known_manifest["progress"],
+    }
+
+    # query w/ scan id (fails)
+    with pytest.raises(
+        requests.exceptions.HTTPError,
+        match=re.escape(
+            f"404 Client Error: Not Found for url: {rc.address}/scan/manifest/{scan_id}"
+        ),
+    ):
+        await rc.request("GET", f"/scan/manifest/{scan_id}")
+
+    # query by event id (none)
+    resp = await rc.request("GET", f"/event/{event_id}")
+    assert not resp["scan_ids"]  # no matches
+
+    # query result (still exists)
+    resp = await rc.request("GET", f"/scan/result/{scan_id}")
+    assert last_known_result == resp
+
+
+async def _delete_result(
+    rc: RestClient,
+    # event_id: str,
+    scan_id: str,
+    last_known_manifest: dict[str, Any],
+) -> None:
+    # delete result
+    resp = await rc.request("DELETE", f"/scan/result/{scan_id}")
+    assert resp == {
+        "scan_id": scan_id,
+        "is_deleted": True,
+        "json_dict": last_known_manifest["json_dict"],
+    }
+
+    # query result (fails)
+    with pytest.raises(
+        requests.exceptions.HTTPError,
+        match=re.escape(
+            f"404 Client Error: Not Found for url: {rc.address}/scan/result/{scan_id}"
+        ),
+    ):
+        await rc.request("GET", f"/scan/result/{scan_id}")
+
+
+########################################################################################
+
+
+async def test_00(server: Callable[[], RestClient]) -> None:
+    """Test normal scan creation and retrieval."""
+    rc = server()
+    event_id = "abc123"
+
+    #
+    # EMPTY DB
+
+    # query by event id
+    resp = await rc.request("GET", f"/event/{event_id}")
+    assert not resp["scan_ids"]  # no matches
+
+    #
+    # LAUNCH SCAN
+    scan_id = await _launch_scan(rc, event_id)
+
+    #
+    # ADD PROGRESS
+    manifest = await _do_progress(rc, event_id, scan_id, 10)
+
+    #
+    # SEND RESULT
+    result = await _send_result(rc, scan_id, manifest)
+
+    #
+    # DELETE MANIFEST
+    await _delete_manifest(rc, event_id, scan_id, manifest, result)
+
+    #
+    # DELETE RESULT
+    await _delete_result(rc, scan_id, manifest)
+
 
     #
     # DELETE MANIFEST
