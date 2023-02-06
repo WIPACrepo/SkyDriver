594218cb6f910c943f144efbeb569717db8562b8
Ric Evans <ric@evans-jacquez.com>
2023-02-06 11:08:23 -0600

    flake8

--- tests/integration/test_rest_routes.py
+++ tests/integration/test_rest_routes.py
@@ -534,8 +534,8 @@ async def test_01__bad_data(server: Callable[[], RestClient]) -> None:
             rc,
             scan_id,
             event_metadata=dict(
-                run_id=run_id,
-                event_id=event_id,
+                run_id=event_metadata["run_id"],
+                event_id=event_metadata["event_id"],
                 event_type="funky",
                 mjd=23423432.3,
                 is_real_event=IS_REAL_EVENT,
