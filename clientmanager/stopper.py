a6945893e85d92dec42612b6e77bbebfba5e4066
Ric Evans <ric@evans-jacquez.com>
2023-03-17 11:11:17 -0500

    only use cluster-id for removing jobs

--- clientmanager/stopper.py
+++ clientmanager/stopper.py
@@ -2,12 +2,10 @@
 
 
 import argparse
-from typing import Any
 
 import htcondor  # type: ignore[import]
-from rest_tools.client import RestClient
 
-from . import condor_tools, utils
+from . import condor_tools
 from .config import LOGGER
 
 
@@ -20,25 +18,6 @@ def attach_sub_parser_args(sub_parser: argparse.ArgumentParser) -> None:
     )
 
 
-def get_cluster_info(
-    skydriver_rc: RestClient,
-    scan_id: str,
-    collector: str,
-    schedd: str,
-    cluster_id: int,
-) -> dict[str, Any]:
-    """Get info from SkyDriver describing the condor cluster."""
-    resp = skydriver_rc.request_seq("GET", f"/scan/manifest/{scan_id}")
-    for cluster in resp["condor_clusters"]:
-        if (
-            cluster["cluster_id"] == cluster_id
-            and cluster["schedd"] == schedd
-            and cluster["collector"] == collector
-        ):
-            return cluster  # type: ignore[no-any-return]
-    raise ValueError(f"Cluster Not Found: {cluster_id=}, {schedd=}, {collector=}")
-
-
 def stop(args: argparse.Namespace) -> None:
     """Main logic."""
     LOGGER.info(
@@ -47,26 +26,13 @@ def stop(args: argparse.Namespace) -> None:
 
     # make connections -- do now so we don't have any surprises
     schedd_obj = condor_tools.get_schedd_obj(args.collector, args.schedd)
-    skydriver_rc, scan_id = utils.connect_to_skydriver()
-    if not skydriver_rc:
-        raise RuntimeError("There must be a connection to SkyDriver to stop jobs.")
-
-    # get jobs (reconstruct 'htcondor.Submit' object)
-    cluster = get_cluster_info(
-        skydriver_rc,
-        scan_id,
-        args.collector,
-        args.schedd,
-        args.cluster_id,
-    )
-    submit_obj = htcondor.Submit(cluster["submit_dict"])  # pylint:disable=no-member
-    jobs = condor_tools.get_job_classads(submit_obj, cluster["jobs"], args.cluster_id)
+    # skydriver_rc, scan_id = utils.connect_to_skydriver()
 
     # Remove jobs -- may not be instantaneous
     LOGGER.info("Requesting removal...")
     schedd_obj.act(
         htcondor.JobAction.Remove,  # pylint:disable=no-member
-        jobs,
+        f"ClusterId == {args.cluster_id}",
         reason="Requested by SkyDriver",
     )
 
