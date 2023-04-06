e415ae8ea5ad4ccfe53de1b6a330a39c2a1d0246
Ric Evans <ric@evans-jacquez.com>
2023-04-06 16:34:11 -0500

    sort env vars

--- clientmanager/config.py
+++ clientmanager/config.py
@@ -14,15 +14,17 @@ class EnvConfig:
     """Environment variables."""
 
     # pylint:disable=invalid-name
-    SKYSCAN_LOG_THIRD_PARTY: str = "WARNING"
-    EWMS_TMS_S3_URL: str = ""
-    EWMS_TMS_S3_ACCESS_KEY: str = ""
-    EWMS_TMS_S3_SECRET_KEY: str = ""
-    SKYSCAN_SKYDRIVER_SCAN_ID: str = ""
+    CLIENT_STARTER_WAIT_FOR_STARTUP_JSON: int = 60
     CONDOR_TOKEN: str = ""
     EWMS_PILOT_QUARANTINE_TIME: int = 0
-    CLIENT_STARTER_WAIT_FOR_STARTUP_JSON: int = 60
+    EWMS_TMS_S3_ACCESS_KEY: str = ""
+    EWMS_TMS_S3_SECRET_KEY: str = ""
+    EWMS_TMS_S3_URL: str = ""
+
+    # piggy-back scanner env vars
+    SKYSCAN_LOG_THIRD_PARTY: str = "WARNING"
     SKYSCAN_SKYDRIVER_ADDRESS: str = ""
+    SKYSCAN_SKYDRIVER_SCAN_ID: str = ""
 
 
 ENV = from_environment_as_dataclass(EnvConfig)
