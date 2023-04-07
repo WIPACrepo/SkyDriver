d59d38f3a9886180abbfbfe3204b3da7120e3695
Ric Evans <ric@evans-jacquez.com>
2023-04-07 15:05:44 -0500

    call it `EWMS_TMS_S3_ACCESS_KEY_ID`

--- clientmanager/utils.py
+++ clientmanager/utils.py
@@ -61,7 +61,7 @@ def s3ify(filepath: Path) -> S3File:
     """Put the file in s3 and return info about it."""
     if not (
         ENV.EWMS_TMS_S3_URL
-        and ENV.EWMS_TMS_S3_ACCESS_KEY
+        and ENV.EWMS_TMS_S3_ACCESS_KEY_ID
         and ENV.EWMS_TMS_S3_SECRET_KEY
         and ENV.EWMS_TMS_S3_BUCKET
         and ENV.SKYSCAN_SKYDRIVER_SCAN_ID
@@ -73,7 +73,7 @@ def s3ify(filepath: Path) -> S3File:
         "s3",
         "us-east-1",
         endpoint_url=ENV.EWMS_TMS_S3_URL,
-        aws_access_key_id=ENV.EWMS_TMS_S3_ACCESS_KEY,
+        aws_access_key_id=ENV.EWMS_TMS_S3_ACCESS_KEY_ID,
         aws_secret_access_key=ENV.EWMS_TMS_S3_SECRET_KEY,
     )
     bucket = ENV.EWMS_TMS_S3_BUCKET
