19157a3dcf2cfd859506fa6a55def37b5e4733fe
Ric Evans <ric@evans-jacquez.com>
2023-04-14 11:30:33 -0500

    fix sanity

--- tests/unit/test_sanity.py
+++ tests/unit/test_sanity.py
@@ -1,49 +1,25 @@
 """Test that everything is where we think it is."""
 
 import inspect
-from pprint import pprint
 
 from rest_tools.server import RestHandler
 from skydriver import rest_handlers
-
-REST_METHODS = {"post", "get", "put", "patch", "delete"}
 
 
 def test_00__rest_handlers() -> None:
     """Dir-check all the REST handlers."""
 
     known_handlers = {
-        rest_handlers.MainHandler: (
-            r"/$",
-            ["post"],
-        ),
-        rest_handlers.RunEventMappingHandler: (
-            r"/scans$",
-            ["get"],
-        ),
-        rest_handlers.ScanLauncherHandler: (
-            r"/scan$",
-            ["post"],
-        ),
-        rest_handlers.ScanHandler: (
-            r"/scan/(?P<scan_id>\w+)$",
-            ["get", "delete"],
-        ),
-        rest_handlers.ScanManifestHandler: (
-            r"/scan/(?P<scan_id>\w+)/manifest$",
-            ["get", "patch"],
-        ),
-        rest_handlers.ScanResultHandler: (
-            r"/scan/(?P<scan_id>\w+)/result$",
-            ["get", "put"],
-        ),
+        rest_handlers.MainHandler: r"/$",
+        rest_handlers.RunEventMappingHandler: r"/scans$",
+        rest_handlers.ScanLauncherHandler: r"/scan$",
+        rest_handlers.ScanHandler: r"/scan/(?P<scan_id>\w+)$",
+        rest_handlers.ScanManifestHandler: r"/scan/(?P<scan_id>\w+)/manifest$",
+        rest_handlers.ScanResultHandler: r"/scan/(?P<scan_id>\w+)/result$",
     }
 
     # search for all known handlers
-    for handler, (route, methods) in known_handlers.items():
-        pprint(dir(handler))
-        assert all(x in dir(handler) for x in methods)
-        assert not any(x in dir(handler) for x in REST_METHODS - set(methods))
+    for handler, route in known_handlers.items():
         assert handler.ROUTE == route  # type: ignore[attr-defined]  # base type does not have ROUTE
 
     # find
