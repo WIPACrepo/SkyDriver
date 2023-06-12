2331a5799503b954e8e1074abcf828c6e86a607c
Ric Evans <ric@evans-jacquez.com>
2023-06-12 16:37:15 -0700

    typo

--- tests/integration/test_backlog_runner.py
+++ tests/integration/test_backlog_runner.py
@@ -124,7 +124,7 @@ async def test_10(
 
     for i in range(N_JOBS + 1):
         resp = await rc.request("POST", "/scan", POST_SCAN_BODY)
-        print(await rc.request("GET", "/scan/backlog"))
+        print(await rc.request("GET", "/scans/backlog"))
         if i == N_JOBS - 2:  # late enough that it won't be started yet by runner
             await rc.request("DELETE", f"/scan/{resp['scan_id']}")
 
