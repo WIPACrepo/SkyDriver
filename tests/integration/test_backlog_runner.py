a21ab95c806eb765da63f3e4009d1b3cbf52e6e6
Ric Evans <ric@evans-jacquez.com>
2023-06-12 14:28:50 -0700

    add front loaded test pt-2

--- tests/integration/test_backlog_runner.py
+++ tests/integration/test_backlog_runner.py
@@ -117,7 +117,7 @@ async def test_00(kapitsj_mock: Mock, server: Callable[[], RestClient]) -> None:
     rc = server()
     await rc.request("POST", "/scan", POST_SCAN_BODY)
 
-    await asyncio.sleep(skydriver.config.ENV.SCAN_BACKLOG_RUNNER_DELAY * 1.1)
+    await asyncio.sleep(skydriver.config.ENV.SCAN_BACKLOG_RUNNER_DELAY * 1.01)
     kapitsj_mock.assert_called_once()
 
     # need a rest route for seeing backlog
@@ -131,8 +131,8 @@ async def test_01(kapitsj_mock: Mock, server: Callable[[], RestClient]) -> None:
     for _ in range(5):
         await rc.request("POST", "/scan", POST_SCAN_BODY)
 
-    for _ in range(5):
-        await asyncio.sleep(skydriver.config.ENV.SCAN_BACKLOG_RUNNER_DELAY * 1.1)
-        kapitsj_mock.assert_called_once()
+    for i in range(5):
+        await asyncio.sleep(skydriver.config.ENV.SCAN_BACKLOG_RUNNER_DELAY * 1.01)
+        kapitsj_mock.call_count = i + 1
 
     # need a rest route for seeing backlog
