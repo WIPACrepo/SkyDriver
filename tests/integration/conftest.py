7bd13c221d97b63b3475b65da03c17a9d53f8ca0
Ric Evans <ric@evans-jacquez.com>
2023-06-26 15:34:35 -0700

    add missing keys

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -4,7 +4,6 @@
 import asyncio
 import socket
 from typing import Any, AsyncIterator, Callable
-from unittest import mock
 from unittest.mock import Mock
 
 import pytest
@@ -50,6 +49,8 @@ KNOWN_CLUSTERS = {
             "collector": "for-sure.a-collector.edu",
             "schedd": "foobar.schedd.edu",
         },
+        "env_var_dest": "CONDOR_TOKEN",
+        "secret_key": "condor_token_foobar",
     },
     "a-schedd": {
         "orchestrator": "condor",
@@ -57,6 +58,8 @@ KNOWN_CLUSTERS = {
             "collector": "the-collector.edu",
             "schedd": "a-schedd.edu",
         },
+        "env_var_dest": "CONDOR_TOKEN",
+        "secret_key": "a_condor_token",
     },
     "cloud": {
         "orchestrator": "k8s",
@@ -64,6 +67,8 @@ KNOWN_CLUSTERS = {
             "host": "cumulus.nimbus.com",
             "namespace": "stratus",
         },
+        "env_var_dest": "WORKER_K8S_CONFIG_FILE_BASE64",
+        "secret_key": "worker_k8s_config_cloud_file_base64",
     },
 }
 
