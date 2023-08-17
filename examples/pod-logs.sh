de5ffbc6dac13d7d4f38af1efffb5e7e42762310
Ric Evans <ric@evans-jacquez.com>
2023-08-17 10:11:38 -0500

    add helper scripts for cloud k8s

---
+++ examples/pod-logs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x  # turn on debugging
+set -e  # exit on fail
+
+if [ -z "$1" ] ; then
+    echo "Usage: pod-logs.sh SCAN_ID"
+    exit 1
+fi
+
+
+lines=$(kubectl get pods -n icecube-skymap-scanner | grep skyscan-worker.*$1)
+
+
+echo ""
+pods=$(echo "$lines" | awk '{ print $1 }')
+for p in $pods; do
+    kubectl logs $p -n icecube-skymap-scanner --all-containers=true
+    break
+done
