b83de7d0da21468d6e17da311da99701a1e41c37
Ric Evans <ric@evans-jacquez.com>
2023-10-31 14:05:38 -0500

    Cluster-Watching Fix 5

--- clientmanager/condor/watcher.py
+++ clientmanager/condor/watcher.py
@@ -22,31 +22,44 @@ def watch(
 
     while time.time() - start < 24 * 60 * 60:  # only go for 1 day -- TODO smarten
         # class ad
-        ads = schedd_obj.query(
-            f"ClusterId == {cluster_id}",
-            # ["list", "of", "desired", "attributes"],
-        )
-        for i, ad in enumerate(ads):
-            LOGGER.debug(f"class ad #{i}")
-            LOGGER.debug(ad)
+        LOGGER.info("getting query classads...")
+        try:
+            ads = schedd_obj.query(
+                f"ClusterId == {cluster_id}",
+                # ["list", "of", "desired", "attributes"],
+            )
+            for i, ad in enumerate(ads):
+                LOGGER.debug(f"class ad #{i}")
+                LOGGER.debug(ad)
+        except Exception as e:
+            LOGGER.debug(f"error -> {e}")
 
         # histories
-        histories = schedd_obj.history(
-            f"ClusterId == {cluster_id}",
-            [],  # ["list", "of", "desired", "attributes"],
-        )
-        for i, history in enumerate(histories):
-            LOGGER.debug(f"history #{i}")
-            LOGGER.debug(history)
+        LOGGER.info("getting histories...")
+        try:
+            histories = schedd_obj.history(
+                f"ClusterId == {cluster_id}",
+                [],  # ["list", "of", "desired", "attributes"],
+            )
+            for i, history in enumerate(histories):
+                LOGGER.debug(f"history #{i}")
+                LOGGER.debug(history)
+        except Exception as e:
+            LOGGER.debug(f"error -> {e}")
 
         # jobEpochHistory
-        histories = schedd_obj.jobEpochHistory(
-            f"ClusterId == {cluster_id}",
-            [],  # ["list", "of", "desired", "attributes"],
-        )
-        for i, history in enumerate(histories):
-            LOGGER.debug(f"jobEpochHistory #{i}")
-            LOGGER.debug(history)
+        LOGGER.info("getting job epoch histories...")
+        try:
+            histories = schedd_obj.jobEpochHistory(
+                f"ClusterId == {cluster_id}",
+                [],  # ["list", "of", "desired", "attributes"],
+            )
+            for i, history in enumerate(histories):
+                LOGGER.debug(f"jobEpochHistory #{i}")
+                LOGGER.debug(history)
+        except Exception as e:
+            LOGGER.debug(f"error -> {e}")
 
+        # wait
         time.sleep(60)
         LOGGER.info("requesting again...")
