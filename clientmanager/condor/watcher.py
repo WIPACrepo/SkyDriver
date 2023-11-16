5a50bfd03eeac3d4de0e544f82fa76ba94cbede8
Ric Evans <ric@evans-jacquez.com>
2023-11-16 16:05:22 -0600

    Cluster-Watching: stop watching once we consistently get no response

--- clientmanager/condor/watcher.py
+++ clientmanager/condor/watcher.py
@@ -34,6 +34,7 @@ DONE_JOB_STATUSES = [
     ct.job_status_to_str(ct.REMOVED),
     ct.job_status_to_str(ct.COMPLETED),
 ]
+NON_RESPONSE_LIMIT = 10
 
 
 def update_stored_job_attrs(
@@ -134,21 +135,37 @@ def watch(
     }
 
     start = time.time()
+    non_response_ct = 0
 
+    def keep_watching() -> bool:
+        """
+        NOTE - condor may be lagging, so we can't just quit when
+        all jobs are done, since there may be more attrs to be updated.
+        """
+        if not any(  # but only if we have done jobs
+            job_attrs[j]["JobStatus"] in DONE_JOB_STATUSES for j in job_attrs
+        ):
+            return True
+        # condor may occasionally slow down & prematurely return nothing
+        return non_response_ct < NON_RESPONSE_LIMIT  # allow X non-responses
+
+    # WATCHING LOOP
     while (
-        not all(job_attrs[j]["JobStatus"] in DONE_JOB_STATUSES for j in job_attrs)
-        and time.time() - start < 60 * 60 * 24  # TODO - be smarter
+        keep_watching()
+        and time.time() - start < 60 * 60 * 24  # just in case, stop if taking too long
     ):
         classads = iter_job_classads(
             schedd_obj,
             (
                 f"ClusterId == {cluster_id} && "
-                # only care about "older" status jobs if they are running
+                # only care about "older" status jobs if they are RUNNING
                 f"( JobStatus == {ct.RUNNING} || EnteredCurrentStatus >= {int(time.time()) - WATCHER_INTERVAL*5} )"
             ),
             PROJECTION,
         )
+        non_response_ct += 1  # just in case
         for ad, source in classads:
+            non_response_ct = 0
             update_stored_job_attrs(job_attrs, ad, source)
 
         LOGGER.info(f"job statuses ({n_workers=})")
