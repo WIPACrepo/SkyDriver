9071bbb27a03234010e005890717e15b2095abba
Ric Evans <ric@evans-jacquez.com>
2023-11-17 16:03:53 -0600

    update patch

--- skydriver/database/interface.py
+++ skydriver/database/interface.py
@@ -147,10 +147,19 @@ class ManifestClient:
             )
 
         # cluster / clusters
+        # TODO - when TMS is up and running, it will handle cluster updating--remove then
         if not cluster:
             pass  # don't put in DB
         else:
-            upserting["clusters"] = in_db.clusters + [cluster]
+            try:  # find by uuid -> replace
+                idx = next(
+                    i for i, c in enumerate(in_db.clusters) if cluster.uuid == c.uuid
+                )
+                upserting["clusters"] = (
+                    in_db.clusters[:idx] + [cluster] + in_db.clusters[idx + 1 :]
+                )
+            except StopIteration:  # not found -> append
+                upserting["clusters"] = in_db.clusters + [cluster]
 
         # complete
         if complete is not None:
