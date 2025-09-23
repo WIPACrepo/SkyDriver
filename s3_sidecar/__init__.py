4ac383fd86248ac3cb3909169b7bd25b4d2d9252
ric-evans <emejqz@gmail.com>
2025-09-23 16:44:22 -0500

    cleanup init comment

--- s3_sidecar/__init__.py
+++ s3_sidecar/__init__.py
@@ -1,7 +1,4 @@
 """Public init."""
 
-# NOTE: `__version__` is not defined as this package is built using 'setuptools-scm' --
-#   use `importlib.metadata.version(...)` if you need to access version info at runtime
-
 # NOTE: `__version__` is not defined because this package is built using 'setuptools-scm' --
 #   use `importlib.metadata.version(...)` if you need to access version info at runtime.
