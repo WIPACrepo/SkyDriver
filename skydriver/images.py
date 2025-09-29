bdc66b1a838a438a66e0972bd83de074bfeb81c8
ric-evans <emejqz@gmail.com>
2025-09-29 15:29:06 -0500

    misc

--- skydriver/images.py
+++ skydriver/images.py
@@ -31,12 +31,8 @@ class ImageTooOldException(Exception):
 # constants
 
 
-_IMAGE = "skymap_scanner"
-_SKYSCAN_DOCKER_IMAGE_NO_TAG = f"icecube/{_IMAGE}"
-
-SKYSCAN_DOCKERHUB_API_URL = (
-    f"https://hub.docker.com/v2/repositories/{_SKYSCAN_DOCKER_IMAGE_NO_TAG}/tags"
-)
+_IMAGE_NAME = "skymap_scanner"
+_SKYSCAN_DOCKER_IMAGE_NAMESPACE = "icecube"
 
 
 # ---------------------------------------------------------------------------------------
@@ -44,19 +40,17 @@ SKYSCAN_DOCKERHUB_API_URL = (
 
 
 def get_skyscan_cvmfs_apptainer_image_path(
-    tag: str, check_exists: bool = False
+    tag: str,
+    check_exists: bool = False,
 ) -> Path:
     """Get the apptainer image path on CVMFS for 'tag' (optionally, check if it exists)."""
-    return CVMFSRegistryTools(ENV.CVMFS_SKYSCAN_SINGULARITY_IMAGES_DIR).get_image_path(
-        _IMAGE,
-        tag,
-        check_exists,
-    )
+    cvmfs = CVMFSRegistryTools(ENV.CVMFS_SKYSCAN_SINGULARITY_IMAGES_DIR, _IMAGE_NAME)
+    return cvmfs.get_image_path(tag, check_exists)
 
 
 def get_skyscan_docker_image(tag: str) -> str:
     """Get the docker image + tag for 'tag' (assumes it exists)."""
-    return f"{_SKYSCAN_DOCKER_IMAGE_NO_TAG}:{tag}"
+    return f"{_SKYSCAN_DOCKER_IMAGE_NAMESPACE}/{_IMAGE_NAME}:{tag}"
 
 
 # ---------------------------------------------------------------------------------------
@@ -72,8 +66,10 @@ async def min_skyscan_tag_ts() -> float:
 
 @aiocache.cached(ttl=ENV.CACHE_DURATION_DOCKER_HUB)  # fyi: tags can be overwritten
 async def get_info_from_docker_hub(docker_tag: str) -> tuple[dict, str]:
-    """Cache dockerhub api call."""
-    ret = DockerHubRegistryTools(SKYSCAN_DOCKERHUB_API_URL).request_info(docker_tag)
+    """Cache docker hub api call."""
+    docker_hub = DockerHubRegistryTools(_SKYSCAN_DOCKER_IMAGE_NAMESPACE, _IMAGE_NAME)
+    ret = docker_hub.request_info(docker_tag)
+
     await asyncio.sleep(0)  # let pending async tasks do things after http request
     return ret
 
@@ -83,14 +79,14 @@ async def resolve_docker_tag(docker_tag: str) -> str:
     LOGGER.info(f"checking docker tag: {docker_tag}")
 
     # cvmfs is the source of truth
-    tool = CVMFSRegistryTools(ENV.CVMFS_SKYSCAN_SINGULARITY_IMAGES_DIR, _IMAGE)
-    docker_tag = tool.resolve_tag(docker_tag)
+    cvmfs = CVMFSRegistryTools(ENV.CVMFS_SKYSCAN_SINGULARITY_IMAGES_DIR, _IMAGE_NAME)
+    docker_tag = cvmfs.resolve_tag(docker_tag)
     get_skyscan_cvmfs_apptainer_image_path(docker_tag, check_exists=True)  # assurance
 
-    # check that it also exists on docker hub
+    # confirm w/ docker hub
+    # -> check tag exists
     dh_info, _ = await get_info_from_docker_hub(docker_tag)
-
-    # check that the image is not too old
+    # -> check image is not too old
     if DockerHubRegistryTools.parse_image_ts(dh_info) < await min_skyscan_tag_ts():
         raise ImageTooOldException()
 
