627be15e26ba77c2397530256d8365612c8d3c5f
ric-evans <emejqz@gmail.com>
2026-04-23 15:51:20 -0500

    ((temp assert 0)) - more detailed error - pprint

--- skydriver/rest_handlers.py
+++ skydriver/rest_handlers.py
@@ -5,6 +5,7 @@ import dataclasses as dc
 import json
 import logging
 import os
+import pprint
 import time
 import uuid
 from typing import Any, Type, TypeVar, cast
@@ -69,7 +70,7 @@ def _schema_error_to_human_readable(err):  # noqa: C901  # ignore "too complex"
         LOGGER.error(f"schema error: {str(err)}")
         LOGGER.error(
             f"{err.__class__.__name__}: "
-            f"{json.dumps({**vars(err), "instance": "<omitted for brevity>"}, indent=4)}"
+            f"{pprint.pformat({**vars(err), "instance": "<omitted for brevity>"}, indent=4)}"
         )
 
     field_path = ".".join(str(p) for p in err.absolute_path)
