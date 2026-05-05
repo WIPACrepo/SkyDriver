cb38c11c60fef573a23e2a4885560c4e91047346
ric-evans <emejqz@gmail.com>
2026-05-05 16:39:53 -0500

    update prod_tester.main

--- resources/prod_tester/__main__.py
+++ resources/prod_tester/__main__.py
@@ -1,23 +1,8 @@
 """Main entrypoint."""
 
 import asyncio
-import subprocess
-import sys
-from pathlib import Path
 
 from . import test_suit_prod
 
 if __name__ == "__main__":
-    # subprocess.check_call("python -m pip install --upgrade pip".split())
-    # subprocess.check_call(
-    #     [
-    #         sys.executable,
-    #         "-m",
-    #         "pip",
-    #         "install",
-    #         "--upgrade",
-    #         "-r",
-    #         str(Path(__file__).parent / "requirements.txt"),
-    #     ]
-    # )
     asyncio.run(test_suit_prod.main())
